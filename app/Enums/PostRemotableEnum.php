8648e3d6277ec41b595a603e63b4196f33de95c7
Nguyễn Lương Việt Huy <108040632+huyviet0110@users.noreply.github.com>
2022-12-12 23:10:38 +0700

    Update PostRemotableEnum.php

--- app/Enums/PostRemotableEnum.php
+++ app/Enums/PostRemotableEnum.php
@@ -3,10 +3,23 @@
 namespace App\Enums;
 
 use BenSampo\Enum\Enum;
+use Illuminate\Support\Str;
 
 final class PostRemotableEnum extends Enum
 {
     public const REMOTE_ONLY = 1;
     public const OFFICE_ONLY = 2;
-    public const DYNAMIC = 3;
+    public const HYBRID      = 3;
+
+    public static function getArrLowerKey(): array
+    {
+        $data = self::asArray();
+        $arr  = ['all' => 0];
+        foreach ($data as $key => $val) {
+            $index       = Str::lower($key);
+            $arr[$index] = $val;
+        }
+
+        return $arr;
+    }
 }
